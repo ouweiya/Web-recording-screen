d3fe9f90bd352adce7257eb6f4c8b99aa4c61ec8
ouweiya <Hi-Outlok@outlook.com>
2019-07-25 08:38:11 +0800

    ''

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -6,7 +6,7 @@
 // will only see deployed updates on subsequent visits to a page, after all the
 // existing tabs open on the page have been closed, since previously cached
 // resources are updated in the background.
-
+// "homepage": "https://ouweiya.github.io/Web-recording-screen",
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
